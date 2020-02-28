8df4c5cdd05ba9242a439c05e745bb136af2a22c
1025608032 <1025608032@qq.com>
2020-02-28 20:24:01 +0800

    add check cookies

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 ## 沙龙社区
-我的毕业设计
++ 这是我的毕业设计——一个社区网站。  
++ 网站主要模块：博客、论坛、沙龙、资料。  
++ 网站功能有：注册登录、发布博客、参与论坛、查看资料和沙龙信息等功能。
 
 ## 资料
-码匠[网课](https://www.bilibili.com/video/av65117012?p=14)  
+网课[Bilibili](https://www.bilibili.com/video/av65117012?p=14)  
 模板网站[Elasticsearch](https://elasticsearch.cn/explore) [腾讯云社区](https://cloud.tencent.com/developer)  
 Spring入门指导[Spring Guides](https://spring.io/guides)  
 使用Spring MVC服务web内容[Spring web](https://spring.io/guides/gs/serving-web-content/)  
 前端框架[Bootstrap4](http://code.z01.com/)  
+模板引擎[Thymeleaf](https://www.thymeleaf.org/)  
 Maven资料库[MavenRepository](https://mvnrepository.com/)  
 授权应用[Github OAuth](https://developer.github.com/apps/building-oauth-apps/)  
-Http客户端[OkHttp](https://square.github.io/okhttp/)
+Http客户端[OkHttp](https://square.github.io/okhttp/)  
+类图[UML规范](https://mp.weixin.qq.com/s/KR2HCcCoIc-gSDLZ69azYw)  
+菜鸟教程[Runoob](https://www.runoob.com/)  
+MyBatis[MyBatis-Spring-Boot-Starter](https://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/index.html)  
+嵌入式数据库支持[SpringBoot默认连接池](https://docs.spring.io/spring-boot/docs/2.2.4.RELEASE/reference/htmlsingle/#boot-features-embedded-database-support)
 
 
 ## 工具
@@ -17,3 +24,14 @@ Http客户端[OkHttp](https://square.github.io/okhttp/)
 [Git](https://git-scm.com/)  
 [visual-paradigm](https://www.visual-paradigm.com/cn/)  
 [zico](http://ico.z01.com/)  
+
+##开发日志
++ 2020/2/14安装IDEA、Maven、Bootstrap4、Git。
+
++ 2020/2/16 初始化Github仓库，初次使用Spring Boot、Git。
+
++ 2020/2/18 项目需求分析，主页页面划分，练习Bootstrap4。
+
++ 2020/2/22 编写导航栏，使用Github App实现第三方登录功能，用session记录当前登录态。
+
++ 2020/2/23 嵌入H2数据库，实现登录态。
--- src/main/java/com/salon/community/controller/AuthorController.java
+++ src/main/java/com/salon/community/controller/AuthorController.java
@@ -2,6 +2,8 @@ package com.salon.community.controller;
 
 import com.salon.community.dto.AccesstokenDTO;
 import com.salon.community.dto.GithubUser;
+import com.salon.community.mapper.UserMapper;
+import com.salon.community.model.User;
 import com.salon.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorController {
@@ -19,24 +23,36 @@ public class AuthorController {
     @Value("${github.client_id}")
     private String clientID;
     @Value("${github.client_secret}")
-    private String client_secret;
+    private String clientSecret;
     @Value("${github.redirect_uri}")
-    private String redirect_uri;
+    private String redirectUri;
+    @Autowired(required = false)
+    private UserMapper userMapper;
     @GetMapping("/login")
     public String login(@RequestParam(name="code") String code,
                         @RequestParam(name="state") String state,
-                        HttpServletRequest resquest){
+                        HttpServletResponse response ){
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
         accesstokenDTO.setClient_id(clientID);
-        accesstokenDTO.setClient_secret(client_secret);
+        accesstokenDTO.setClient_secret(clientSecret);
         accesstokenDTO.setCode(code);
-        accesstokenDTO.setRedirect_uri(redirect_uri);
+        accesstokenDTO.setRedirect_uri(redirectUri);
         accesstokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accesstokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        //System.out.println(githubUser.getId());
+        //System.out.println(githubUser.getName());
+        if(githubUser != null){
             //登录成功，写cookie和session
-            resquest.getSession().setAttribute("user",user);
+            User user = new User();
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
             //登录失败，重新登录
